4132c7b5503a79ade8aa5b7a6c8716ed923c1323
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-10-08 17:48:52 -0400

    infra: added global company metadata schema with billing and module tracking

---
+++ BusinessAsUsual.Admin/ProvisioningScripts/CreateCompanyMetadata.sql
@@ -0,0 +1,16 @@
+﻿-- Creates the global Companies table if it doesn't already exist
+IF NOT EXISTS (
+    SELECT * FROM INFORMATION_SCHEMA.TABLES
+    WHERE TABLE_NAME = 'Companies'
+)
+BEGIN
+    CREATE TABLE Companies (
+        Id UNIQUEIDENTIFIER PRIMARY KEY,
+        Name NVARCHAR(100) NOT NULL,
+        DbName NVARCHAR(100) NOT NULL,
+        AdminEmail NVARCHAR(100) NOT NULL,
+        BillingPlan NVARCHAR(50) NOT NULL DEFAULT 'Standard',
+        ModulesEnabled NVARCHAR(MAX) NOT NULL DEFAULT 'Employees,Products',
+        CreatedAt DATETIME NOT NULL DEFAULT GETUTCDATE()
+    );
+END
