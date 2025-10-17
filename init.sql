54aa96395dc9980f65fbc2d0d2c1f18a45e639f8
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-10-17 12:30:31 -0400

    I deserve a purple heart

--- init.sql
+++ init.sql
@@ -3,3 +3,18 @@ GO
 
 CREATE DATABASE [TestDb];
 GO
+
+EXEC sp_configure 'remote access', 1;
+RECONFIGURE;
+
+USE [TestDb];
+GO
+
+CREATE TABLE TestPing (Id INT);
+GO
+
+USE [TestDb];
+GO
+
+SELECT name FROM sys.tables;
+GO
