868dc9844ae43a442128fd9d73bcf3b409836089
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-11-05 08:15:31 -0500

    feat(provisioning): wire up MasterDbContext for tenant metadata storage
    - Created MasterDbContext for BusinessAsUsual master database
    - Registered in DI and injected into TenantProvisioningService
    - Persisted TenantMetadata after successful provisioning
    
    #smartCommit #multitenancy #efcore #metadata #serviceLayer

---
+++ BusinessAsUsual.Infrastructure/Persistence/MasterDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BusinessAsUsual.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessAsUsual.Infrastructure.Data;
+
+/// <summary>
+/// DbContext for storing tenant metadata in the master BusinessAsUsual database.
+/// </summary>
+public class MasterDbContext : DbContext
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="options"></param>
+    public MasterDbContext(DbContextOptions<MasterDbContext> options) : base(options) { }
+
+    /// <summary>
+    /// Collection of tenant metadata records
+    /// </summary>
+    public DbSet<TenantMetadata> Tenants { get; set; }
+}
